fac1932d0412ccf72cfdc1d72dfbc221eab8a316
B Kanhu <iamkanhuc@gmail.com>
2024-06-04 09:34:38 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/about" alt="About Page">
+        About
+      </Link>
+    </>
+  );
+}
